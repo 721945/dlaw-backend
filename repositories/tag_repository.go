f92a493ff385abf76b8fa82cc13a56d1d12b37c7
721945 <t.choolerk@gmail.com>
2023-05-29 02:27:05 +0700

    🐛 fix(file_controller.go): change HTTP status code from 201 to 200 in SearchFiles method 🐛 fix(tag_repository.go): add tag.display_name to Select statement to fix error in CountFilesInTagsByFolderId method The HTTP status code in the SearchFiles method was changed from 201 to 200 to reflect the correct status code for a successful request. In the CountFilesInTagsByFolderId method, the tag.display_name was added to the Select statement to fix an error that was occurring when the method was called.

--- repositories/tag_repository.go
+++ repositories/tag_repository.go
@@ -70,7 +70,7 @@ func (r *TagRepository) CountFilesInTags(fileIds []uuid.UUID) (tags []models.Tag
 
 func (r *TagRepository) CountFilesInTagsByFolderId(folderId uuid.UUID) (tags []models.TagCount, err error) {
 	return tags, r.db.DB.Table("tags").
-		Select("tags.id, tags.name, COALESCE(file_counts.count, 0) as count").
+		Select("tags.id, tags.name,tag.display_name, COALESCE(file_counts.count, 0) as count").
 		Joins("LEFT JOIN (SELECT file_tags.tag_id, COUNT(DISTINCT file_tags.file_id) as count FROM file_tags JOIN files ON file_tags.file_id = files.id WHERE files.folder_id = ? GROUP BY file_tags.tag_id) as file_counts ON tags.id = file_counts.tag_id", folderId).
 		Where("tags.show_menu = TRUE").
 		Find(&tags).Error
